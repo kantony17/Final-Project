50cb214b127518f9e6650d06fe5c98d9756a6222
kantony17 <ksenthil@conncoll.edu>
2016-05-08 16:21:33 -0400

    Create TestWishList.java

---
+++ TestWishList.java
@@ -0,0 +1,20 @@
+public class TestWishList {
+	public static void main(String[] args) {
+		WishList w = new WishList();
+		Movie aMovie = new Movie("Captain America: Civil War", 20160506, 10001, 97);
+		Movie bMovie = new Movie("Deadpool", 20160212, 10002, 83);
+		Movie cMovie = new Movie("The Jungle Book", 20160403, 10003, 94);
+		Movie dMovie = new Movie("The Boss", 20160780, 10004, 19);
+		Movie eMovie = new Movie("Iron Man", 20160920, 10005, 95);
+
+		w.addNewMovie(aMovie);
+/*		w.addNewMovie(bMovie);
+		w.addNewMovie(cMovie);
+		w.addNewMovie(dMovie);
+		w.addNewMovie(eMovie);*/
+
+		w.printList();
+
+	}
+	
+}
