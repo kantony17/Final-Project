2f2158826ccb41176a420680eeff20f5bc71f95a
astutzma <astutzma@conncoll.edu>
2016-05-09 20:01:50 -0400

    Add files via upload

--- MainMenu.java
+++ MainMenu.java
@@ -4,10 +4,10 @@
 -admins have options to acess and edit the movie database
 */
 
-
+import java.io.*;
 import java.util.Scanner;
 
-public class MainMenu{
+public class MainMenu implements java.io.Serializable{
 
 	//instance variables
 	private String passwordA = "netflixAdmin";
@@ -21,6 +21,16 @@ public class MainMenu{
 
 	//constructor 
 	public MainMenu(){
+		/*
+		//
+		try{
+
+		}
+		//else serialize it
+		else{
+
+		}
+			*/
 		try{
 			Scanner f = new Scanner(System.in);
 			System.out.println("\n--------Welcome to NetFlix!--------\nPlease Login");
@@ -32,12 +42,13 @@ public class MainMenu{
 					System.out.print("Password:  ");
 					String password = p.next();
 					if (password.equals(passwordA)){
+						System.out.println("\nWelcome Admin!\n");
 						int adminInput = 5;
 						while (adminInput != 4){
 							try{
 								Scanner t = new Scanner(System.in);
-								System.out.println("\nWelcome Admin\n Please select an option from the list below:\n1. Add a movie to the database.\n2. View least-rated movie in the database.\n3. View all movies in database(by order of release date).\n4. Pick this option to quit.\n");
-								System.out.print("Please select an option 1-4: ");
+								System.out.println("Please select an option from the list below (1-4):\n1. Add a movie to the database.\n2. View least-rated movie in the database.\n3. View all movies in database(by order of release date).\n4. Pick this option to quit.\n");
+								System.out.print("Option: ");
 								adminInput = t.nextInt();
 								if (adminInput == 1){ //add movie to the database
 									moviesDatabase.centralAdd(); //central add makes sure it changes in all of the data structures 
@@ -63,7 +74,6 @@ public class MainMenu{
 									}
 
 								}
-							
 								else if (adminInput == 3){ //view all movies in the database
 									System.out.println("The folowing movies are available for viewing in the database:\n");
 									moviesBST.traverseMovieTree();
@@ -87,21 +97,22 @@ public class MainMenu{
 					System.out.print("Password:  ");
 					String password = o.next();
 					if (password.equals(passwordC)){
+						System.out.println("\nWelcome Customer!\n");
 						try{
 							Scanner s = new Scanner(System.in);
-							System.out.println("What would you like to do today? \n 1. Play a Movie \n 2. See movies in your wish list \n 3. Add a new movie to your wish list \n 4. Delete a movie from your wishlist \n 5. View your recently watched movies \n");
-							System.out.print("Please select an option 1-5: ");
+							System.out.println("What would you like to do today (1-6)? \n1. Play a Movie \n2. See movies in your wish list \n3. Add a new movie to your wish list \n4. Delete a movie from your wishlist \n5. View your recently watched movies \n6. Quit\n");
+							System.out.print("Option: ");
 							int userInput = s.nextInt();
 							if (userInput == 1){ //play first movie of wishlist
 								wishlist.firstMovie();
 								try{
 									Scanner e = new Scanner(System.in); //give them the option to delete after watching
 									System.out.println("Would you like to delete this movie from the wishlist after you've seen it? Yes or No");
-									String userInput5 = e.next();
-									if (userInput5.equals("Yes")){
+									String userInput5 = e.next().toLowerCase();
+									if (userInput5.equals("yes")){
 										wishlist.delete(wishlist.firstMovie().getID());
 									}
-									else if (userInput5.equals("No")){
+									else if (userInput5.equals("no")){
 										System.out.println("This movie will remain in your wish list.");;
 									}
 								}
@@ -142,11 +153,11 @@ public class MainMenu{
 				}
 			}
 		}
-		catch(IllegalArgumentException i){
-			System.out.println("You did not enter one of the choices. Choose Again");
-			i.printStackTrace(); 
+		while (username != "admin" || username != "customer"){
+			System.out.println("That username does not exist. Please try again.");
+			String username = f.next();
 		}
-	}
+	}	
 }
 
 
