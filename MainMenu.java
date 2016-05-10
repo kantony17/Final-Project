ffc133b4c75bb359eb3c8afec7d028d6fbfa259b
scarley18 <scarley@conncoll.edu>
2016-05-10 19:14:12 -0400

    Update MainMenu.java

--- MainMenu.java
+++ MainMenu.java
@@ -2,7 +2,6 @@
 -the main menu implements scanners in order to distinguish between admins and customers
 -customers have acess to edit their wishlists
 -admins have options to acess and edit the movie database
--new customers can create profiles and then become customers!
 */
 
 import java.io.*;
@@ -13,7 +12,7 @@ public class MainMenu implements java.io.Serializable{
 	//instance variables
 	private String passwordA = "netflixAdmin";
 	private String passwordC = "netflixCust";
-	private String username;
+	private int username;
 	private String password;
 	WishList wishlist = new WishList();
 	MovieHashTable_ID moviesHash = new MovieHashTable_ID();
@@ -35,22 +34,20 @@ public class MainMenu implements java.io.Serializable{
 
 		}
 			*/
-		username = "";
+		username = 0;
 		Boolean returningCust = false;
 		//System.out.println("Test before while loop");
-		while ((!(username.equals("admin"))) && (!(username.equals("1"))) && ((returningCust == false))){
-			//System.out.println("test begining of while loop");
+		while ((username != 1) && (username != 2) && ((returningCust == false))){
 			Scanner f = new Scanner(System.in);
-			System.out.println("\n-----------Welcome to NetFlix!-----------\nPlease Login Below\nAdmins and Returning Customers enter approriate username, or new Customers please press 1.");
-			System.out.print("Username: ");
-			username = f.next();
-			username = username.replaceAll("\\s+",""); //removes white space and characters
+			System.out.println("\n-----------Welcome to NetFlix!-----------\nPlease Login Below\nAdmins press 1 and Returning Customers enter last 4-digits of your CCN, and new Customers please press 2.");
+			System.out.print("Login: ");
+			username = f.nextInt();
 			returningCust = customerDatabase.hasCustomer(username);
-			if ((!(username.equals("admin"))) && (!(username.equals("1"))) && ((returningCust == false))){
+			if ((username != 1) && (username != 2) && ((returningCust == false))){
 				System.out.println("\nPlease try again, that wasn't an option!\n");
 			}
 		}
-		if (username.equals("admin")){
+		if (username == 1){
 			password = "";
 			while (!(password.equals(passwordA))){
 				Scanner p = new Scanner(System.in);
@@ -111,16 +108,16 @@ public class MainMenu implements java.io.Serializable{
 				}	
 			}
 		}
-		else if ((returningCust == true) || username.equals("1")){
+		else if ((returningCust == true) || (username == 2)){
 			Scanner o = new Scanner(System.in);
-			if (username.equals("1")){
+			if (username == 2){
 				customerDatabase.addCustomer();
-				System.out.println("Enter your new Username (email) to begin!   ");
-				username = o.next();
+				System.out.println("Enter the  last four digits of your CCN to begin!   ");
+				username = o.nextInt();
 			}
-
+			Scanner a = new Scanner(System.in);
 			System.out.print("Password:  ");
-			password = o.next();
+			password = a.next();
 			System.out.println("password printed" + password);
 
 			customerDatabase.printCustomers(); // TEST check to see if they exist in the database
@@ -129,58 +126,58 @@ public class MainMenu implements java.io.Serializable{
 				System.out.println("Nice try... You're password is wrong. Why don't you enter a different password.");
 				System.out.print("Password:  ");
 				password = o.next();
-				password = password.replaceAll("\\s+",""); //removes white space and characters
+				if ((customerDatabase.passwordMatch(username, password)) == false){
+					System.out.println("\n Sorry that password doesnt match the CCN you entered, please try again\n")
 			}
 
-			if (password.equals(passwordC)){
-				System.out.println("\nWelcome Customer!\n");
-				int userInput9 = 0;
-				while (userInput9 != 6){
-					Scanner s = new Scanner(System.in);
-					System.out.println("What would you like to do today? Choose and option (1-6) and press enter.\n1. Play a Movie\n2. See movies in your wish list\n3. Add a new movie to your wish list\n4. Delete a movie from your wishlist\n5. View your recently watched movies\n6. Quit\n");
-					System.out.print("Option: ");
-					userInput9 = s.nextInt();
+			System.out.println("\nWelcome Customer!\n");
+			
+			int userInput9 = 0;
+			while (userInput9 != 6){
+				Scanner s = new Scanner(System.in);
+				System.out.println("What would you like to do today? Choose and option (1-6) and press enter.\n1. Play a Movie\n2. See movies in your wish list\n3. Add a new movie to your wish list\n4. Delete a movie from your wishlist\n5. View your recently watched movies\n6. Quit\n");
+				System.out.print("Option: ");
+				userInput9 = s.nextInt();
 
-					if (userInput9 == 1){ //play first movie of wishlist
-						Movie play = wishlist.firstMovie();
-						System.out.println("You are now about to 'watch': " + play.getTitle());
-						try{
-							Scanner e = new Scanner(System.in); //give them the option to delete after watching
-							System.out.println("Would you like to delete this movie from the wishlist after you've seen it? Yes or No");
-							String userInput5 = e.next().toLowerCase();
-							if (userInput5.equals("yes")){
-								wishlist.delete(wishlist.firstMovie().getID());
-							}
-							else if (userInput5.equals("no")){
-								System.out.println("This movie will remain in your wish list.");;
-							}
+				if (userInput9 == 1){ //play first movie of wishlist
+					Movie play = wishlist.firstMovie();
+					System.out.println("You are now about to 'watch': " + play.getTitle());
+					try{
+						Scanner e = new Scanner(System.in); //give them the option to delete after watching
+						System.out.println("Would you like to delete this movie from the wishlist after you've seen it? Yes or No");
+						String userInput5 = e.next().toLowerCase();
+						if (userInput5.equals("yes")){
+							wishlist.delete(wishlist.firstMovie().getID());
 						}
-						catch(IllegalArgumentException k){
-							System.out.println("You did not enter one of the choices. Choose Again");
-							k.printStackTrace();
+						else if (userInput5.equals("no")){
+							System.out.println("This movie will remain in your wish list.");;
 						}
 					}
-					else if (userInput9 == 2){ //print wishlist
-						wishlist.printList();
+					catch(IllegalArgumentException k){
+						System.out.println("You did not enter one of the choices. Choose Again");
+						k.printStackTrace();
 					}
-					else if (userInput9 == 3){ //add movie to wishlist
-						Scanner n = new Scanner(System.in);
-						System.out.println("Enter the ID of the movie you would like to add:   ");
-						int userInput2 = n.nextInt();
-						Movie movie = moviesHash.lookUp(userInput2);
-						wishlist.addNewMovie(movie);
-					}
-					else if (userInput9 == 4){ //delete movie from wishlist
-						Scanner d = new Scanner(System.in);
-						System.out.println("Enter the ID of the movie you would like to delete:    ");
-						int userInput3 = d.nextInt();
-						wishlist.delete(userInput3);
-					}
-					else if (userInput9 == 5){ //print recently watched movies
-						moviesStack.printRecentlyWatchedStack();
-					}
-					//6 quits the program, anything else prompts within options while loop
 				}
+				else if (userInput9 == 2){ //print wishlist
+					wishlist.printList();
+				}
+				else if (userInput9 == 3){ //add movie to wishlist
+					Scanner n = new Scanner(System.in);
+					System.out.println("Enter the ID of the movie you would like to add:   ");
+					int userInput2 = n.nextInt();
+					Movie movie = moviesHash.lookUp(userInput2);
+					wishlist.addNewMovie(movie);
+				}
+				else if (userInput9 == 4){ //delete movie from wishlist
+					Scanner d = new Scanner(System.in);
+					System.out.println("Enter the ID of the movie you would like to delete:    ");
+					int userInput3 = d.nextInt();
+					wishlist.delete(userInput3);
+				}
+				else if (userInput9 == 5){ //print recently watched movies
+					moviesStack.printRecentlyWatchedStack();
+				}
+				//6 quits the program, anything else prompts within options while loop
 			}
 		}
 	}	
